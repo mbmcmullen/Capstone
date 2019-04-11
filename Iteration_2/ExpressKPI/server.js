f34fe104844a31fe1f6a2b78f930d76d4ebf055d
Senderhauf <centerhauf@gmail.com>
2019-04-11 10:14:59 -0500

    comments in areas that require changes, anticipating merge conflicts

--- Iteration_2/ExpressKPI/server.js
+++ Iteration_2/ExpressKPI/server.js
@@ -9,34 +9,17 @@ var mqtt = require('mqtt')
 const PORT = 80
 const HOST = '0.0.0.0'
 
+//#region MQTT connection
+
 var client = mqtt.connect('tcp://mosquitto-broker:1883')
-var clientCurMessages = {
-    client_1: null, 
-    client_2: null,
-    client_3: null
+var clientMessages = {
+    client_1: {
+        time: null, 
+        value: null
+    },     
+    client_2: null,     
+    client_3: null      
 } 
-
-function mqttSource(){
-    
-    return Observable.create( function(observer){
-        const interval = setInterval( () => {
-            //console.log(`DEBUG mqttSource.next: ${clientCurMessages.client_1}`)
-            if (clientCurMessages.client_1 && clientCurMessages.client_1 % 3 == 0){
-                observer.error()
-            }
-            observer.next(clientCurMessages.client_1)
-        }, 4000)
-        return () => clearInterval(interval)
-    })
-}
-
-let subject1 = new Source(mqttSource, () => {console.log('DEBUG: Server MqttSource ERROR')})
-subject1.subscribe(x => console.log(`DEBUG: subject.subscribe ${x}`))
-
-const app = express()
-app.get('/', (req, res) => {
-    res.send('Placeholder for Node status monitoring App.\n')
-})
 
 client.on('connect', () => {
     client.subscribe('current_time/client_1')
@@ -49,17 +32,72 @@ client.on('message', (topic, message) => {
     console.log(`DEBUG: Server message - topic: ${topic}, message: ${message}`)
     switch(topic) {
         case 'current_time/client_1':
-            clientCurMessages.client_1 = message.toString();
+            clientMessages.client_1 = message.toString();
+        case 'current_time/client_2':
+
         default :
     }
-    //clientCurMessages.push({"topic": topic.toString(), "message": message.toString()})
+    //clientMessages.push({"topic": topic.toString(), "message": message.toString()})
     //client.end()
 })
 
+//#endregion
+
+//#region RxJS
+
+function mqttSource(){
+    
+    return Observable.create( function(observer){
+        /**
+         * TODO: 
+         *      Check whether client message is current ( 'not stale': within specified time interval)
+         *      If value is deemed 'stale' begin sending value objects with 'status': 'invalid'
+         *          else continue sending current data with value objects containing 'status': 'valid'
+         *      Keep count of number of 'stale/invalid' data values 
+         *          clear count if new 'current/valid' value is recieved
+         *          if count exceeds predefined limit then send 'complete' notification
+         *      Define way to message client stating that current value is in 'stale/invalid' mode
+         *          responsibility for handling error message from server is put on the client node. 
+         *              if client wants to restart or keep sending error/ not sending current values, that's it's choice
+         *      
+         */ 
+
+        const interval = setInterval( () => {
+            
+            if (clientMessages.client_1 && clientMessages.client_1 % 3 == 0){
+                observer.error()
+            }
+            observer.next(clientMessages.client_1)
+        }, 4000)
+        return () => clearInterval(interval)
+    })
+}
+
+/**
+ * TODO: 
+ *      Note the current implementation of Source takes in an Error Handler function
+ *          but this error handler function cannot access any status variables within the Obervable's scope
+ *          This could become an issue when a specific error handler needs to access the number of 'invalid' 
+ *              data values to determine if it should send a 'complete' notification
+ */
+let subject1 = new Source(mqttSource, () => {console.log('DEBUG: Server MqttSource ERROR')})
+subject1.subscribe(x => console.log(`DEBUG: subject.subscribe ${x}`))
+
+//#endregion
+
+//#region  API
+
+const app = express()
+app.get('/', (req, res) => {
+    res.send('Placeholder for Node status monitoring App.\n')
+})
+
 app.get('/clients_all', (req, res) => {
-    console.log(`DEBUG: Server clientCurMessages: ${clientCurMessages}`)
-    res.send({length: clientCurMessages.length, messages: clientCurMessages})
+    console.log(`DEBUG: Server clientMessages: ${clientMessages}`)
+    res.send({length: clientMessages.length, messages: clientMessages})
 })
 
 app.listen(PORT, HOST)
 console.log(`DEBUG: Running on http://${HOST}:${PORT}`)
+
+//#endregion
